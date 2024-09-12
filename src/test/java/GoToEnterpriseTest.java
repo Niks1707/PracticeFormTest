a7475aa74c21dcd9fd49d33c68eacae1b28e533e
Veronika Genkel <genkel1707@gmail.com>
2024-09-12 22:02:49 +0200

    deleted empty line

--- src/test/java/GoToEnterpriseTest.java
+++ src/test/java/GoToEnterpriseTest.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
